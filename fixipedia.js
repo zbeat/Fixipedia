ce0b5641b7e93e8571230db70ce46befc2c453e8
Zack Beatty <zackbeatty@gmail.com>
2012-10-17 00:06:39 -0400

    Sharing an extension I built on Saturday.

---
+++ fixipedia.js
@@ -0,0 +1,12 @@
+var lat, lng, mapWidget, thisGovType;
+var supportedGovTypes = ['Federal district','State Capital','City and county','City','Town','Borough','Village','CDP','Ghost town','Unincorporated community'];
+$(document).ready(function(){
+	lat = $(".geo").html().split(';')[0];
+	lng = $(".geo").html().split(';')[1];
+	mapWidgetSrc = 'http://seeclickfix.com/issues/iframe?token=f4a72ef691cdab12f3f665b8c1b2886c71c3c2df&lat=' + lat + '&lng=' + lng + '&num_results=50&zoom=13';
+	mapWidget = '<iframe width="100%" height="300" src="' + mapWidgetSrc + '" scrolling="no" marginheight="0" frameborder="0" marginwidth="0" allowtransparency="true" hspace="0" vspace="0"></iframe>';
+	thisGovType = $(".category a").text();
+	if($.inArray(thisGovType, supportedGovTypes) !== -1) {
+		$('table.vcard tr:last').after('<tr class="mergedtoprow"><th colspan="2">SeeClickFix Issues<br/>' + mapWidget + '<br/><a href="' + mapWidgetSrc + '" target="_blank" style="font-weight:normal;">large map</a></th></tr>');
+	}
+});
