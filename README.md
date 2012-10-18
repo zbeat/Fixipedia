13628bdc128e6d5b8650449ddc9600210fbdc9de
Zack Beatty <zackbeatty@gmail.com>
2012-10-17 21:37:14 -0400

    Minor improvements.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 Fixipedia
 =========
 
-A simple Chrome extension that add a SeeClickFix issue map to any 'place' (city, town, borough, village, etc) article on Wikipedia.
+A simple Chrome extension that adds a SeeClickFix issue map to any 'place' article on Wikipedia. 
+
+# Currently supported places include:
+* Federal district
+* State Capital
+* City and county
+* City
+* Borough
+* Town
+* Village
+* Hamlet
+* CDP (census-designated place)
+* Unincorporated community
+* Ghost town
+
+Please send me URLs to a place page that does not work. Current non-functioning examples:
+* http://en.wikipedia.org/wiki/Peoria,_Illinois
--- fixipedia.js
+++ fixipedia.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 	lng = $(".geo").html().split(';')[1];
 	mapWidgetSrc = 'http://seeclickfix.com/issues/iframe?token=f4a72ef691cdab12f3f665b8c1b2886c71c3c2df&lat=' + lat + '&lng=' + lng + '&num_results=50&zoom=13';
 	mapWidget = '<iframe width="100%" height="300" src="' + mapWidgetSrc + '" scrolling="no" marginheight="0" frameborder="0" marginwidth="0" allowtransparency="true" hspace="0" vspace="0"></iframe>';
-	thisGovType = $(".category a").text();
+	//thisGovType = $(".category a").text();
+	thisGovType = $(".category").text();
 	if($.inArray(thisGovType, supportedGovTypes) !== -1) {
 		$('table.vcard tr:last').after('<tr class="mergedtoprow"><th colspan="2">SeeClickFix Issues<br/>' + mapWidget + '<br/><a href="' + mapWidgetSrc + '" target="_blank" style="font-weight:normal;">large map</a></th></tr>');
 	}
